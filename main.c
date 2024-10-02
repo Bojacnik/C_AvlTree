bada26002618e5a07939c8c6f8ef611eb47e762b
Petr Novy <xnovype00@vutbr.cz>
2024-10-02 23:02:15 +0200

    update: changed test to check functionality

--- main.c
+++ main.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
 
 #include "avltree.h"
-
+#include "string.h"
 
 int main(void) {
-    AvlNode* root = avl_create_node(10);
-    root = avl_insert_avltree(root, 40);
-    root = avl_insert_avltree(root, 30);
-    root = avl_insert_avltree(root, 20);
-    root = avl_insert_avltree(root, 25);
-    root = avl_insert_avltree(root, 45);
-    root = avl_insert_avltree(root, 50);
-    root = avl_insert_avltree(root, 35);
 
-    root = avl_insert_avltree(root, 150);
-    root = avl_insert_avltree(root, 156);
-    root = avl_insert_avltree(root, 159);
-    root = avl_insert_avltree(root, 157);
-    root = avl_insert_avltree(root, 158);
-    root = avl_insert_avltree(root, 151);
 
-    const AvlNode* found = avl_find_avltree(root, 45);
-    if (found != NULL)
-    {
-        printf("Found: %d\n", found->data);
-    }
-    else
-    {
-        puts("Not found");
-    }
-
+    Symtable_Node* root = avl_create_node(avl_create_row("negr"));
+    avl_insert_avltree(root, avl_create_row("adolf"));
+    avl_insert_avltree(root, avl_create_row("pica"));
+    avl_insert_avltree(root, avl_create_row("zmrd"));
+    avl_insert_avltree(root, avl_create_row("stalin"));
+    avl_insert_avltree(root, avl_create_row("churchill"));
+    avl_insert_avltree(root, avl_create_row("babis"));
+    avl_insert_avltree(root, avl_create_row("joever"));
 
     avl_print_inorder(root);
     puts("");
     avl_print_postorder(root);
     puts("");
     avl_print_preorder(root);
-    return 0;
+
+     return 0;
 }
 
 
